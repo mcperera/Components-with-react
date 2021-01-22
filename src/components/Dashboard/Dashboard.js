fbfed8c7b7e925195afe27e754d11cf192f3a3bd
Madushan Perera <madushanperera3817@gmail.com>
2021-01-22 15:00:40 +0530

    main color change

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,34 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import { Header } from "../";
 
-import { dashboardWrapper } from "./Dashboard.module.css";
+import {
+  dashboardWrapper,
+  homeStyle,
+  reactStyle,
+  cssStyle,
+} from "./Dashboard.module.css";
 
 function Dashboard({ children }) {
+  const { pathname } = useLocation();
+
+  console.log(pathname);
+
+  // Ternary Operators --> lol
+  const pageStyle =
+    pathname === "/"
+      ? homeStyle
+      : pathname === "/react"
+      ? reactStyle
+      : pathname === "/css"
+      ? cssStyle
+      : null;
+
   return (
     <div className={dashboardWrapper}>
       <Header />
-      <main>{children}</main>
+      <main className={pageStyle}>{children}</main>
     </div>
   );
 }
