1066d9c8577f872f21071f8ee5e4fb1d9aed9b0a
Madushan Perera <madushanperera3817@gmail.com>
2021-02-01 19:12:41 +0530

    partially done

--- src/_projects/react_components/Search/Search.js
+++ src/_projects/react_components/Search/Search.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainContainer } from "../../../components";
-
 import { mapMain, searchContainer, btn } from "./Search.module.css";
 
-function search() {
+const rootURL = "https://api.github.com";
+
+function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const [showUser, setShowUser] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    console.log(searchValue);
+    // fetch(`${rootURL}/users/${event.target.value}`)
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     console.log(result);
+    //     setShowUser(result);
+    //   })
+    //   .catch((error) => console.log(error));
   };
 
   return (
@@ -14,14 +25,27 @@ function search() {
       <h2 className="pageTitle">- Search -</h2>
       <div className={searchContainer}>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Search something.." />
+          <input
+            type="text"
+            placeholder="Search for.."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
           <button className={btn} type="submit">
             Search
           </button>
         </form>
+        <p>{searchValue}</p>
+        <p>{showUser}</p>
+        {/* {showUser.login && (
+          <>
+            <p>I'm here : {showUser.login}</p>{" "}
+            <img src={showUser.avatar_url} alt={showUser.login} />
+          </>
+        )} */}
       </div>
     </MainContainer>
   );
 }
 
-export default search;
+export default Search;
