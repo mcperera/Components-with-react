3c393bb9653bf822e63b3602d640632ed80c7009
Madushan Perera <madushanperera3817@gmail.com>
2021-02-02 11:23:47 +0530

    demo search

--- src/_projects/react_components/Search/Search.js
+++ src/_projects/react_components/Search/Search.js
@@ -1,48 +1,65 @@
 import React, { useState } from "react";
 import { MainContainer } from "../../../components";
-import { mapMain, searchContainer, btn } from "./Search.module.css";
+import {
+  mapMain,
+  searchContainer,
+  btn,
+  searchContent,
+} from "./Search.module.css";
 
-const rootURL = "https://api.github.com";
+//const rootURL = "https://api.github.com";
+const rootURL = "https://api.itbook.store/1.0/";
 
 function Search() {
   const [searchValue, setSearchValue] = useState("");
-  const [showUser, setShowUser] = useState("");
+  const [showBooks, setShowBooks] = useState([]);
+  const [showError, setShowError] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSearch = (event) => {
     event.preventDefault();
-    console.log(searchValue);
-    // fetch(`${rootURL}/users/${event.target.value}`)
-    //   .then((res) => res.json())
-    //   .then((result) => {
-    //     console.log(result);
-    //     setShowUser(result);
-    //   })
-    //   .catch((error) => console.log(error));
+    console.log("log ", event.target.value);
+    setSearchValue(event.target.value);
+    fetch(`${rootURL}/search/${event.target.value}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("result ", result);
+        const { books } = result;
+        if (books.length > 0) {
+          setShowBooks(books);
+          setShowError(false);
+        } else {
+          setShowError(true);
+        }
+      })
+      .catch((error) => {
+        console.log("error-->", error);
+        setShowError(true);
+      });
   };
 
   return (
     <MainContainer className={mapMain}>
       <h2 className="pageTitle">- Search -</h2>
       <div className={searchContainer}>
-        <form onSubmit={handleSubmit}>
+        <form>
           <input
             type="text"
-            placeholder="Search for.."
+            placeholder="Search for IT books.."
             value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+            onChange={handleSearch}
           />
-          <button className={btn} type="submit">
+          {/* <button className={btn} type="submit">
             Search
-          </button>
+          </button> */}
         </form>
-        <p>{searchValue}</p>
-        <p>{showUser}</p>
-        {/* {showUser.login && (
-          <>
-            <p>I'm here : {showUser.login}</p>{" "}
-            <img src={showUser.avatar_url} alt={showUser.login} />
-          </>
-        )} */}
+        <div className={searchContent}>
+          {showError && <p>No Books Found!</p>}
+          <ul>
+            {showBooks?.map((book) => {
+              return <li>{book.title}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </MainContainer>
   );
